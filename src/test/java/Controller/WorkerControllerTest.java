c65c36f5bf64ca000dd2a9ca8ba3590dfcb1471c
Julietta <vald.viktor44@mail.ru>
2022-03-27 23:55:51 +0300

    Added files, for testing with helping postman

---
+++ src/test/java/Controller/WorkerControllerTest.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkerControllerTest {
+
+    @Test
+    void createWorker() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void readFromId() {
+    }
+
+    @Test
+    void updateWorker() {
+    }
+
+    @Test
+    void deleteWorker() {
+    }
+}
